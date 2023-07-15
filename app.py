620b77f2a134515f24dffe4b3dbed3bcf3a0cf93
himani0550 <himani0550@gmail.com>
2023-07-15 13:08:01 +0530

    adding new one

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app=Flask(__name__)
 @app.route('/', methods=['GET'])
 def helloWorld():
     return'helloworld'
+@app.route('/aboutus', methods=['GET'])
+def helloWorld():
+    return'welcome back'
 if __name__=="__main__":
     app.run(port=3000,debug='True')
